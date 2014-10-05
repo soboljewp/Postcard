b99b2816ba7422edc36aabb48f437dd15cbc8397
Patrick Dawson <patrick.soboljew@gmail.com>
2014-10-05 10:55:34 +0200

    Wired up enterNameTextField.

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,18 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
     }
     
 }
